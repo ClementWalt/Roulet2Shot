c59a2933888769478f146ef6a630d505ea806a7f
Clément Walther <clement.walther@etudiant.univ-reims.fr>
2018-12-19 09:27:21 +0100

    Màj 19/12/2018 09:27

--- script/check.js
+++ script/check.js
@@ -1,18 +1,22 @@
 
 function check(){
   var valeur=0;
-  
+
   if (document.getElementById('t1').checked) {
     valeur = document.getElementById('t1').value;
+    document.querySelector("#choix").style.display = "None";
   }
   if (document.getElementById('t2').checked) {
     valeur = document.getElementById('t2').value;
+    document.querySelector("#choix").style.display = "None";
   }
   if (document.getElementById('t3').checked) {
     valeur = document.getElementById('t3').value;
+    document.querySelector("#choix").style.display = "None";
   }
   if (document.getElementById('t4').checked) {
     valeur = document.getElementById('t4').value;
+    document.querySelector("#choix").style.display = "None";
   }
   roue(valeur);
 
